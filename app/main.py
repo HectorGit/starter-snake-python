3a81387fc7c59bb03fefb4d8b576c49c21ba0840
HectorGit <hectorandres.pv@gmail.com>
2019-02-28 21:35:27 -0600

    trying to fix crashing into walls

--- app/main.py
+++ app/main.py
@@ -168,13 +168,13 @@ def move():
     # here possibly add a way to fix that 
     # you crash into walls
 
-    if y_diff>0 and firstMove['y'] < height and firstMove ['y'] > 0 : 
+    if y_diff>0 and firstMove['y'] < height-1 and firstMove ['y'] > 0 : 
         direction = 'up'
-    if y_diff<0 and firstMove['y'] < height and firstMove ['y'] > 0: 
+    if y_diff<0 and firstMove['y'] < height-1 and firstMove ['y'] > 0: 
         direction = 'down'
-    if x_diff>0 and firstMove['x'] < width and firstMove ['x'] > 0: 
+    if x_diff>0 and firstMove['x'] < width-1 and firstMove ['x'] > 0: 
         direction = 'left'
-    if x_diff<0 and firstMove['x'] < width and firstMove ['x'] > 0: 
+    if x_diff<0 and firstMove['x'] < width-1 and firstMove ['x'] > 0: 
         direction = 'right'
 
     return move_response(direction)
